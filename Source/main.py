fa37abb4a0e7502be0a3a62064a80ff5fc001172
Gellifree <forge.delfadortitan@gmail.com>
2021-08-17 20:31:42 +0200

    Üres main.py hozzáadása

---
+++ Source/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: 2021 Kovács Norbert <mfw.kovacs.norbert@gmail.com>
+#
+
+def main():
+	print("Helló Világ!")
+
+if __name__ == "__main__":
+	main()
